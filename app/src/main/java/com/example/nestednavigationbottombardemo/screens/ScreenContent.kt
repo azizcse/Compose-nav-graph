944312ff43a1a1287361785534e8df4bc293b75a
Aziz <azizul@w3engineers.com>
2022-08-16 13:16:39 +0600

    commit

--- app/src/main/java/com/example/nestednavigationbottombardemo/screens/ScreenContent.kt
+++ app/src/main/java/com/example/nestednavigationbottombardemo/screens/ScreenContent.kt
@@ -1,21 +1,25 @@
 package com.example.nestednavigationbottombardemo.screens
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
-fun ScreenContent(navController: NavHostController,name: String, onClick: () -> Unit) {
-    Scaffold (topBar = {
+fun ScreenContent(navController: NavHostController, name: String, onClick: () -> Unit) {
+    Scaffold(topBar = {
         TopAppBar(
             title = { Text("Top App Bar") },
             backgroundColor = MaterialTheme.colors.primary,
@@ -25,17 +29,24 @@ fun ScreenContent(navController: NavHostController,name: String, onClick: () -> 
                 }
             }
         )
-    }){
+    }) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                modifier = Modifier.clickable { onClick() },
-                text = name,
-                fontSize = MaterialTheme.typography.h3.fontSize,
-                fontWeight = FontWeight.Bold
-            )
+            Card(
+                modifier = Modifier.padding(20.dp),
+                border = BorderStroke(1.dp, Color.Blue),
+                elevation = 10.dp
+            ) {
+                Text(
+                    modifier = Modifier.clickable { onClick() }.padding(10.dp),
+                    text = name,
+                    fontSize = MaterialTheme.typography.h3.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
         }
     }
 
